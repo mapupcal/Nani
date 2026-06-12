227f0cbb076cc109b895e7a3202eb6756256b2db
Mapupcal <mapupcal@hotmail.com>
2026-06-13 03:32:40 +0800

    [runtime] add Directory implementation, no testing.

--- nani/runtime/directory.cpp
+++ nani/runtime/directory.cpp
@@ -1,1 +1,136 @@
 ﻿#include "directory.h"
+#include <filesystem>
+using namespace std;
+namespace nani::runtime
+{
+	Directory::Directory(const u8string_view& basePath)
+		: m_basePath(basePath)
+	{
+
+	}
+
+	Directory::Directory()
+	{
+		auto cwd = filesystem::current_path();
+		m_basePath = cwd.u8string();
+	}
+
+	Directory::~Directory()
+	{
+
+	}
+
+	bool Directory::Exists() const
+	{
+		error_code ec;
+		return filesystem::exists(m_basePath, ec) && filesystem::is_directory(m_basePath, ec);
+	}
+
+	bool Directory::Exists(const u8string_view& dirPath) const
+	{
+		Directory testAbsoluteDir(dirPath);
+		if (testAbsoluteDir.IsAbsolute())
+			return testAbsoluteDir.Exists();
+
+		u8string absPath = AbsolutePath();
+		absPath.append(u8"/");
+		absPath.append(dirPath);
+		Directory dir(absPath);
+		return dir.Exists();
+	}
+
+	bool Directory::CD(const u8string_view& dirPath)
+	{
+		if (!Exists(dirPath))
+			return false;
+
+		Directory testAbsoluteDir(dirPath);
+		if (testAbsoluteDir.IsAbsolute())
+		{
+			if (testAbsoluteDir.Exists())
+			{
+				m_basePath = testAbsoluteDir.AbsolutePath();
+				return true;
+			}
+			return false;
+		}
+
+		m_basePath.append(u8"/");
+		m_basePath.append(dirPath);
+		return true;
+	}
+
+	bool Directory::CreateDirectory(const u8string_view& dirPath)
+	{
+		Directory testAbsoluteDir(dirPath);
+		if (testAbsoluteDir.IsAbsolute())
+		{
+			if (testAbsoluteDir.Exists())
+				return true;
+			error_code ec;
+			return filesystem::create_directories(dirPath, ec);
+		}
+
+		u8string absPath = AbsolutePath();
+		absPath.append(u8"/");
+		absPath.append(dirPath);
+		Directory dir(absPath);
+		if (dir.Exists())
+			return true;
+
+		error_code ec;
+		return filesystem::create_directories(dir.AbsolutePath(), ec);
+	}
+
+	bool Directory::IsAbsolute() const
+	{
+		filesystem::path path(m_basePath);
+		return path.is_absolute();
+	}
+
+	u8string Directory::AbsolutePath() const
+	{
+		if (IsAbsolute())
+			return m_basePath;
+
+		error_code ec;
+		return filesystem::weakly_canonical(m_basePath, ec).u8string();
+	}
+
+	bool Directory::RemoveFile(const u8string_view& fileName)
+	{
+		u8string filePath = FilePath(u8string(fileName));
+		error_code ec;
+		return filesystem::remove(filePath, ec);
+	}
+
+	bool Directory::RemoveDirectory(const u8string_view& dirPath)
+	{
+		Directory dir(AbsolutePath());
+		if (!dir.CD(dirPath))
+			return false;
+		error_code ec;
+		return filesystem::remove_all(dir.AbsolutePath(), ec);
+	}
+
+	bool Directory::RemoveRecursively()
+	{
+		error_code ec;
+		return filesystem::remove_all(AbsolutePath(), ec);
+	}
+
+	u8string Directory::FilePath(const u8string& fileName) const
+	{
+		return AbsolutePath().append(u8"/").append(fileName);
+	}
+
+	vector<u8string> Directory::EnumFiles(const vector<u8string>& extensions) const
+	{
+		return vector<u8string>();
+	}
+
+	vector<u8string> Directory::EnumDirectories() const
+	{
+		return vector<u8string>();
+	}
+}
--- nani/runtime/directory.h
+++ nani/runtime/directory.h
@@ -4,6 +4,27 @@ namespace nani::runtime
 {
 	class NANI_RUNTIME_API Directory
 	{
+	public:
+		Directory(const std::u8string_view& basePath);
+		Directory();
+		~Directory();
 
+	public:
+		bool Exists() const;
+		bool Exists(const std::u8string_view& dirPath) const;
+		bool CD(const std::u8string_view& dirPath);
+		bool CreateDirectory(const std::u8string_view& dirPath);
+		bool IsAbsolute() const;
+		std::u8string AbsolutePath() const;
+		bool RemoveFile(const std::u8string_view& fileName);
+		bool RemoveDirectory(const std::u8string_view& dirPath);
+		bool RemoveRecursively();
+		std::u8string FilePath(const std::u8string& fileName) const;
+
+		std::vector<std::u8string> EnumFiles(const std::vector<std::u8string>& extensions) const;
+		std::vector<std::u8string> EnumDirectories() const;
+
+	private:
+		std::u8string m_basePath;
 	};
 }
